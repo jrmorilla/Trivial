f4d0fa64d9a1b2c1b44cff1d0b4f9655ccb9bcaf
jrmorilla <morillarodriguezjoaquin@gmail.com>
2024-01-07 19:25:01 +0100

    actualizacion modo carrera para que sea más divertido

--- funciones_funcionalidad.py
+++ funciones_funcionalidad.py
@@ -19,23 +19,19 @@ def jugar_modo_normal(num_preguntas):
         contador = contador + 1
     print(f"Has obtenido {puntuacion} puntos")
 
-def jugar_modo_carrera(num_preguntas, categoria, dificultad):
-    json_raw = funciones_preguntas.get_json_raw_modo_carrera(num_preguntas, categoria, dificultad)
-    contador = 1
-    puntuacion = 0
-    for i in json_raw:
-        print(f"Tu puntuación es: {puntuacion} puntos")
-        print("\n")
-        opciones = funciones_preguntas.construir_opciones_enumeradas(i)
-        print(f"Pregunta {contador}: {funciones_preguntas.sacar_pregunta(i)}")
-        for indice, elemento in enumerate(opciones):
-            print(f"{indice + 1}. {elemento}")
-        print("\n")
-        respuesta = int(input("Elige un número: "))
-        if funciones_preguntas.determinar_acierto(i, opciones, respuesta):
-            puntuacion = puntuacion + 1
-        contador = contador + 1
-    print(f"Has obtenido {puntuacion} puntos")
+def jugar_modo_carrera(contador, num_preguntas, categoria, dificultad):
+    json_raw = funciones_preguntas.get_json_raw_modo_carrera(1, categoria, dificultad)
+    print("\n")
+    opciones = funciones_preguntas.construir_opciones_enumeradas(json_raw[0])
+    print(f"Pregunta {contador}: {funciones_preguntas.sacar_pregunta(json_raw[0])}")
+    for indice, elemento in enumerate(opciones):
+        print(f"{indice + 1}. {elemento}")
+    print("\n")
+    respuesta = int(input("Elige un número: "))
+    if funciones_preguntas.determinar_acierto(json_raw[0], opciones, respuesta):
+        return 1
+    else:
+        return 0
 
 def seleccion_categoria():
     categorias = ['Books', 'Film', 'Music', 'Musicals & Theatres', 'Television', 'Video Games', 'Board Games', 'Nature',
--- inicializacion.py
+++ inicializacion.py
@@ -1,3 +1,6 @@
+import funciones_funcionalidad
+
+
 def iniciar_juego():
     print("Bienvenido al Trivial!\nPor favor, indique qué quiere hacer:")
     print("1. Modo normal")
@@ -9,10 +12,26 @@ def iniciar_juego():
     return respuesta
 
 def iniciar_modo_carrera():
-    pass
+    print("Vas a jugar al modo carrera. Este modo de juego consiste en acertar todas las preguntas que puedas sobre un tema en específico. Tú eliges el tema y la dificultad. Tienes tres vidas.")
+    dificultad = funciones_funcionalidad.seleccion_dificultad()
+    categoria = funciones_funcionalidad.seleccion_categoria()
+    vidas = 3
+    aciertos = 0
+    contador = 1
+    while vidas > 0:
+        print(f"Tienes {vidas} vidas")
+        if funciones_funcionalidad.jugar_modo_carrera(contador, 1, categoria, dificultad) == 1:
+            aciertos = aciertos + 1
+        else:
+            vidas = vidas - 1
+        contador = contador + 1
+    print("¡Te has quedado sin vidas!")
+    print(f"Has acertado {aciertos} preguntas de la categoría {categoria}")
+
 
 def get_num_preguntas():
     print("Bienvenido al Trivial! Por favor, inserte el número de preguntas que quieres jugar: ")
     num_preguntas = int(input("Introduce un número: "))
     return num_preguntas
 
+iniciar_modo_carrera()
